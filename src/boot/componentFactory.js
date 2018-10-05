ae7e0e32d564913ca2640bcc7d4ff7487a8ad67a
Stef Coenen <stef.coenen@gmail.com>
2018-10-05 10:46:42 +0200

    Added atomic design organisation;

--- src/boot/componentFactory.js
+++ src/boot/componentFactory.js
@@ -1,19 +1,30 @@
-import Welcome from "../app/components/Welcome";
-import Hero from "../app/components/Hero";
-import Navigation from "../app/components/Navigation";
-import Footer from "../app/components/Footer";
-import Section from "../app/components/Section";
-import JssCard from "../app/components/Card";
-import JssImage from "../app/components/Image";
+// Atoms
+import JssImage from "../app/components/atoms/Image";
+
+// Molecules
+import Hero from "../app/components/molecules/Hero";
+import Section from "../app/components/molecules/Section";
+import JssCard from "../app/components/molecules/Card";
+
+// Organisms
+import Welcome from "../app/components/organisms/Welcome";
+import Navigation from "../app/components/organisms/Navigation";
+import Footer from "../app/components/organisms/Footer";
 
 const components = new Map();
-components.set("Welcome", Welcome);
-components.set("Hero", Hero);
-components.set("Navigation", Navigation);
-components.set("Section", Section);
-components.set("Footer", Footer);
-components.set("JssCard", JssCard);
+
+// Atoms
 components.set("JssImage", JssImage);
+
+// Molecules
+components.set("Hero", Hero);
+components.set("Section", Section);
+components.set("JssCard", JssCard);
+
+// Organsism
+components.set("Welcome", Welcome);
+components.set("Navigation", Navigation);
+components.set("Footer", Footer);
 
 const componentFactory = componentName => components.get(componentName);
 
