2b00123fb46e690e02b161b3d0095a53f8adcf59
Stef Coenen <stef.coenen@gmail.com>
2018-10-09 11:35:31 +0200

    Cleanup of layout;

--- src/Layout.js
+++ src/Layout.js
@@ -4,12 +4,6 @@ import Helmet from 'react-helmet';
 
 import './assets/scss/jss-headless.scss';
 
-// Using bootstrap is completely optional. It's used here to provide a clean layout for samples,
-// without needing extra CSS in the sample app. Remove it in package.json as well if it's removed here.
-// import 'bootstrap/dist/css/bootstrap.css';
-// import './assets/app.css';
-// import logo from './assets/sc_logo.png';
-
 /*
   APP LAYOUT
   This is where the app's HTML structure and root placeholders should be defined.
@@ -17,39 +11,6 @@ import './assets/scss/jss-headless.scss';
   All routes share this root layout by default (this could be customized in RouteHandler),
   but components added to inner placeholders are route-specific.
 */
-
-// This is boilerplate navigation for sample purposes. Most apps should throw this away and use their own navigation implementation.
-// Most apps may also wish to use GraphQL for their navigation construction; this sample does not simply to support disconnected mode.
-// let Navigation = ({ t, i18n }) => (
-//     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
-//         <h5 className="my-0 mr-md-auto font-weight-normal">
-//             <NavLink to="/" className="text-dark">
-//                 <img alt="Sitecore" />
-//             </NavLink>
-//         </h5>
-//         <nav className="my-2 my-md-0 mr-md-3">
-//             <a
-//                 className="p-2 text-dark"
-//                 href="https://jss.sitecore.net"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//             >
-//                 {t('Documentation')}
-//             </a>
-//             <NavLink to="/styleguide" className="p-2 text-dark">
-//                 {t('Styleguide')}
-//             </NavLink>
-//             <NavLink to="/graphql" className="p-2 text-dark">
-//                 {t('GraphQL')}
-//             </NavLink>
-//         </nav>
-//     </div>
-// );
-
-// inject dictionary props (`t`) into navigation so we can translate it
-// NOTE: using this is needed instead of using i18next directly to keep
-// the component state updated when i18n state (e.g. current language) changes
-// Navigation = translate()(Navigation);
 
 const Layout = ({ route }) => (
     <React.Fragment>
